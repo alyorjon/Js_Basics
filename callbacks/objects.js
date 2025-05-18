2c5b1af1a0f263902880967f923f31ca5de130fc
alyorjon <alyor6328@gmail.com>
2025-05-18 16:58:41 +0500

    Refactor person object structure and enhance logging for properties

--- callbacks/objects.js
+++ callbacks/objects.js
@@ -4,10 +4,24 @@ const person={
     job:'Full stack developer',
     hobies:{
         sport:"Football",
-        games:'UFC'
+        gamess:'UFC',
+        games:'UFC1',
     }
 }
 
 for (let k in person){
+    if (typeof person[k]==='object'){
+        for (let i in person[k]){
+            
+        console.log(`Property :${k}, Values ${person[k][i]}`);
+        }
+    }else{
+        
     console.log(`Property :${k} : Values ${person[k]}`);
+    }
 }
+
+// for in -- faqat object uchun
+// for of --faqat  massive uchun 
+
+console.log(Object.keys(person));
